9e7b6af4b061ac399129e5e66be6656564b92eac
UmbraCodex <the-shadow-skript@gmail.com>
2026-02-21 09:10:24 -0500

    02: updated response to provide optional metadata & verbose output

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-import os
+import os, argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 if not api_key:
-    raise RuntimeError("environment variable wasn't found")
+    raise RuntimeError("api key wasn't found")
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
 
 client = genai.Client(api_key=api_key)
-generate_content = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+if not response.usage_metadata:
+    raise RuntimeError("model's usage metadata wasn't found")
 
 def main():
-    print("Hello from ai-agent!")
-    print(generate_content.text)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
     main()
