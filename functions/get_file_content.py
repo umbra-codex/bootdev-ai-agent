02efae36545c2d9e51b23aa236f15e777915b0d5
Umbra <umbra.codex1@gmail.com>
2026-05-01 13:07:59 -0400

    04: added get_file_content and test_get_file_content

---
+++ functions/get_file_content.py
@@ -0,0 +1,23 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+  working_dir_abs = os.path.abspath(working_directory)
+  target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
+
+  if os.path.commonpath([working_dir_abs, target_file]) != working_dir_abs:
+    return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+  
+  if not os.path.isfile(target_file):
+    return f'Error: File not found or is not a regular file: "{file_path}"'
+
+  with open(target_file, "r") as f:
+    try:
+      file_content_string = f.read(MAX_CHARS)
+      if f.read(1):
+        file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+    except OSError as ex:
+      return f'Error: {ex}'
+
+  return file_content_string
