6a0f91a6c208d391daffb7df6e93cebc4c335206
Umbra <umbra.codex1@gmail.com>
2026-05-01 14:54:31 -0400

    07: added system_prompt & available_functions for calling

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os, argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,7 +17,11 @@ args = parser.parse_args()
 
 client = genai.Client(api_key=api_key)
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=messages,
+    config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt),
+)
 if not response.usage_metadata:
     raise RuntimeError("model's usage metadata wasn't found")
 
@@ -24,7 +30,11 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response:\n{response.text}")
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f"Response:\n{response.text}")
 
 if __name__ == "__main__":
     main()
