86651dfebec7ba539002669cfafd776306555349
Umbra <umbra.codex1@gmail.com>
2026-05-03 00:40:55 -0400

    09: added function calling support with get_file_content, get_files_info, run_python_file, & write_file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -31,8 +31,23 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     if response.function_calls:
+        function_results = []
         for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, args.verbose)
+
+            if not function_call_result.parts:
+                raise Exception("call_function returned a Content object with no parts")
+            
+            if function_call_result.parts[0].function_response is None:
+                raise Exception("function_response is None")
+            
+            if function_call_result.parts[0].function_response.response is None:
+                raise Exception("function_response.response is None")
+            
+            function_results.append(function_call_result.parts[0])
+
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
     else:
         print(f"Response:\n{response.text}")
 
